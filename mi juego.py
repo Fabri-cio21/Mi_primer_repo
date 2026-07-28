d13177d227731d6a6c2e251ddc88da701ad6e5a1
Fabricio Villar Ato <100161149@cientifica.edu.pe>
2026-07-27 21:13:50 -0500

    Agregué as.txt y mi juego

---
+++ mi juego.py
@@ -0,0 +1,13 @@
+import os
+
+
+print("INGRESE NOMBRE DEL PROTAGONISTA")
+a = input()
+print(f"""
+      Hola {a}, bienvenido a mi juego,
+      esto es una beta, beta, beta donde 
+      verán el comienzo del desarrollo de 
+      este juego :)   
+""")
+input("\nPresiona ENTER para salir...")
+os.system("pause")
